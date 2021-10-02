1668eb50f0b354a028cba01416134517dbcc315a
umiiii419 <umiiii419@gmail.com>
2021-10-02 07:45:26 +0000

    [#19]Add アクションを記述

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,3 +1,19 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def unsubscribe
+  end
+
+  def withdrawal
+  end
+
 end
