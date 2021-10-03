6ea4b5adee5a32806e2f55a521797b5dfabfb676
umiiii419 <umiiii419@gmail.com>
2021-10-03 23:51:21 +0000

    [#21]Add shopモデルのカラムを追加

--- db/migrate/20211003232127_create_shops.rb
+++ db/migrate/20211003232127_create_shops.rb
@@ -2,6 +2,17 @@ class CreateShops < ActiveRecord::Migration[5.2]
   def change
     create_table :shops do |t|
 
+      t.references :genre,            null: false, foreign_key: true
+      t.string     :name,             null: false
+      t.string     :name_kana,        null: false
+      t.string     :image_id,         null: false
+      t.string     :address,          null: false
+      t.string     :telephone_number, null: false
+      t.string     :opening_hours,    null:false
+      t.string     :regular_holiday,  null: false
+      t.string     :nearest_station,  null: false
+      t.boolean    :is_deleted,       null: false, default: false
+
       t.timestamps
     end
   end
