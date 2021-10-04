f428163a754818cf8261523d7225c4057271c169
umiiii419 <umiiii419@gmail.com>
2021-10-04 02:26:13 +0000

    [#21]Add Refile.secret_keyを設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '22ed7ac883db8c9ebb44260fecbc6ac540e12fc55da1ed0ffabe9e22d490f0fce3557867dee264c7d467120f24682a302327435a0cdb24043fd1c114385c8c6f'
