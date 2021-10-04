e26c51f9c3f0a93cfa2e669bcbf5f8a37c50eb4d
umiiii419 <umiiii419@gmail.com>
2021-10-04 07:44:41 +0000

    [#25]Fix 新規登録機能を修正

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,6 +1,7 @@
 class Public::ReviewsController < ApplicationController
 
   def new
+    @shop = Shop.find(params[:shop_id])
     @review = Review.new
   end
 
@@ -14,12 +15,27 @@ class Public::ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to review_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(
+      :user_id,
+      :shop_id,
+      :title,
+      :body,
+      :image,
+      :rate
+    )
   end
 
 end
