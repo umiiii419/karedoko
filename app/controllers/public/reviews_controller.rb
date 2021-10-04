ba6f45ac9d7a98e629abc21e5df97b67a5a944a5
umiiii419 <umiiii419@gmail.com>
2021-10-04 05:59:27 +0000

    [#25]Add public/reviewsコントローラーにアクションを記述

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,2 +1,24 @@
 class Public::ReviewsController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
