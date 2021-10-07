ac8233a74b2b92c2d93fda5eeb643ccdc3820439
umiiii419 <umiiii419@gmail.com>
2021-10-07 08:19:34 +0000

    [#38]Add 編集機能を作成

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -6,12 +6,21 @@ class Admins::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admins_users_path
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:profile_image, :nickname, :email, :introduction, :is_deleted)
   end
 
 end
