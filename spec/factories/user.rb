e2d43ed608f430a34c41d49cb2bf3eda994cba35
umiiii419 <umiiii419@gmail.com>
2021-10-13 03:06:02 +0000

    [#67]Add ユーザーログイン前のテストを作成

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.email }
     nickname { Faker::Name.name }
+    email    { Faker::Internet.email }
     password { Faker::Internet.password(min_length :6) }
     password_confirmation { password }
   end
