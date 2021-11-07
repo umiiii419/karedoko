f7a754121806620064543d787a98fd3351daeaac
umiiii419 <umiiii419@gmail.com>
2021-11-07 04:09:19 +0000

    [#92]Fix adminとuserそれぞれ別の認証キーを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :validatable, :authentication_keys => [:email]
 
   def active_for_authentication?
     super && (is_deleted == false)
