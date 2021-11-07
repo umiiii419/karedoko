d610e313d9200a4f6fd6305a7437c747ab7618c3
umiiii419 <umiiii419@gmail.com>
2021-11-07 05:34:00 +0000

    [#92]Fix 管理者のログイン後のリダイレクト先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    #後でadminとユーザーに分ける
-    root_path
+    case resource
+    when Admin
+      admins_shops_path
+    when User
+      root_path
+    end
   end
 
   def after_sign_out_path_for(resource)
